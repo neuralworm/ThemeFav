750074b5beaffcca623a4d6e952b9c191042f0cd
neuralworm <jakemillerdev@gmail.com>
2023-11-20 21:24:04 -0800

    Docs.

--- src/ThemeExtJSON.ts
+++ src/ThemeExtJSON.ts
@@ -4,6 +4,7 @@ export interface ThemeExtJSON {
     uiTheme: string,
     id?: string
 }
+// WHEN USING NAMES FOUND VIA EXTENSION SEARCH, ID'S MUST BE PRIORITIZED OVER LABELS
 export const createThemeExtJSON = (label: string, path: string, uiTheme: string, id?: string|null): ThemeExtJSON => {
     return{
         label: label,
